8a66af27e0c06c00f899195c23b4fb3670e13ee1
Jesus Oviedo Riquelme <j92riquelme@gmail.com>
2025-09-13 17:57:51 -0300

    Se corrige la configuración de reintentos en el recurso `google_cloud_run_v2_job` en `main.tf`, cambiando `retries` a `max_retries` y `max_retries_duration` a `max_retry_duration` para alinearse con la nomenclatura correcta, mejorando la claridad y consistencia del código.

--- terraform/modules/cloud_run_job/main.tf
+++ terraform/modules/cloud_run_job/main.tf
@@ -9,8 +9,8 @@ resource "google_cloud_run_v2_job" "job" {
         image = var.image
         args  = var.args
       }
-      retries=2
-      max_retries_duration="10m"
+      max_retries  = 2
+      max_retry_duration  = "10m"
     }
   }
 }
